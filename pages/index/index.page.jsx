097ec27a2a5a3dfd5594b1ff0acf03c0cd23e202
Pushpendra Singh <psnitsri01@gmail.com>
2023-08-29 22:37:16 +0530

    Adding index page with responsive support

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -1,5 +1,31 @@
+import Styles from './main.module.css';
+
 const Page = () => {
-  return <h1>Lets Build</h1>;
+  return (
+    <div className={Styles.page}>
+      <main className={Styles.main}>
+        <header className={Styles.header}>
+          <img className={Styles.logo} src='/logo.png' />
+          <p className={Styles.name}>LandingMosaic</p>
+        </header>
+        <section className={Styles.hero}>
+          <h1 className={Styles.heading}>Transforming Ideas into</h1>
+          <h1 className={`${Styles.heading} ${Styles.accent}`}>
+            Captivating Landings
+          </h1>
+          <p className={Styles.info}>
+            Your ultimate destination for captivating landing page inspirations.
+            Immerse yourself in a gallery of cutting-edge design concepts that
+            ignite creativity and transform ideas into captivating digital
+            experiences.
+          </p>
+        </section>
+        <section>
+          <a href='/neo-brutalism'>Neo Brutalism</a>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export { Page };
--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -19,6 +19,8 @@ async function render(pageContext: PageContextServer) {
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html>
       <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>${title}</title>
       </head>
       <body>
