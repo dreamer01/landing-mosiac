edb24644b241744870bde9d594b8b9eca26c5e4a
Pushpendra Singh <psnitsri01@gmail.com>
2023-09-17 22:17:24 +0530

    Updating homepage

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -5,10 +5,31 @@ const Page = () => {
     <div className={Styles.page}>
       <main className={Styles.main}>
         <header className={Styles.header}>
-          <img className={Styles.logo} src='/logo.png' />
-          <p className={Styles.name}>LandingMosaic</p>
+          <div className={Styles.flex}>
+            <img className={Styles.logo} src='/logo.png' />
+            <p className={Styles.name}>LandingMosaic</p>
+          </div>
+          <a className={Styles.aboutLink} href='/about'>
+            About
+          </a>
         </header>
         <section className={Styles.hero}>
+          <div className={Styles.heroContent}>
+            <h1 className={Styles.heroHeading}>
+              Make the best first impression
+            </h1>
+            <p>
+              A collection of trendy landing pages, a perfect way to showcase
+              your products and reach your audience. Immerse yourself in a
+              gallery of cutting-edge design concepts, and check what style of
+              design suits your product and brand the best.
+            </p>
+          </div>
+          <div className={Styles.heroImgView}>
+            <img className={Styles.heroImg} src='/pattern.png' />
+          </div>
+        </section>
+        {/* <section className={Styles.display}>
           <h1 className={Styles.heading}>Transforming Ideas into</h1>
           <h1 className={`${Styles.heading} ${Styles.accent}`}>
             Captivating Landings
@@ -19,10 +40,18 @@ const Page = () => {
             ignite creativity and transform ideas into captivating digital
             experiences.
           </p>
-        </section>
-        <section>
-          <a href='/neo-brutalism'>Neo Brutalism</a>
-          <a href='/3d-web'>3D Web</a>
+        </section> */}
+        <section className={Styles.gallery}>
+          <div className={Styles.galleryView}>
+            <a className={Styles.card} href='/neo-brutalism'>
+              <img className={Styles.siteSnapshot} src='/neo-seo.png' />
+              <div className={Styles.siteMeta}>Neo Brutalism</div>
+            </a>
+            <a className={Styles.card} href='/3d-web'>
+              <img className={Styles.siteSnapshot} src='/3d-seo.png' />
+              <div className={Styles.siteMeta}>3D Modern Web</div>
+            </a>
+          </div>
         </section>
       </main>
     </div>
